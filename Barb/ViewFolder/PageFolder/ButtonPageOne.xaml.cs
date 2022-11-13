37b55b865590ec3e0b3b46cdb15559eed6f46e2c
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2022-11-13 16:25:07 +0300

    Добавьте файлы проекта.

---
+++ Barb/ViewFolder/PageFolder/ButtonPageOne.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Barb.ViewFolder.PageFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для ButtonPageOne.xaml
+    /// </summary>
+    public partial class ButtonPageOne : Page
+    {
+        public ButtonPageOne()
+        {
+            InitializeComponent();
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Barb/ViewFolder/PageFolder/ButtonPageTho.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Barb.ViewFolder.PageFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для ButtonPageTho.xaml
+    /// </summary>
+    public partial class ButtonPageTho : Page
+    {
+        public ButtonPageTho()
+        {
+            InitializeComponent();
+        }
+
+        private void NewProductsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NewWorkerButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NewManufacturerButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReportButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
